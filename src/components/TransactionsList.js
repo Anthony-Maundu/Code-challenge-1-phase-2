ff9841ca8f50d2d562e30b696ccc938c27f9721e
valary <valaryoluoch@gmail.com>
2024-08-06 09:01:18 +0300

    list

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
+function TransactionsList(props) {
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,10 +20,13 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+        <Transaction
+          transactions={props.transactions}
+          loading={props.loading}
+        />
       </tbody>
     </table>
   );
 }
-
 export default TransactionsList;
+
